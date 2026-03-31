0ab4a94e0d9dd91a2e5e5823b050df99f0f2e012
ItunyaHorse <5027231008@student.its.ac.id>
2026-03-31 22:55:11 +0700

    feat: refactor codes

--- frontend/app/sign/page.tsx
+++ frontend/app/sign/page.tsx
@@ -2,9 +2,11 @@
 
 export default function SignPage() {
     return(
-        <div id="Keys" className="relative h-screen w-full bg-black">
-            <div>
-                Sign Page
+        <div id="Sign" className="pages">
+            <div className=" flex bg-red-300 h-full items-center text-black rounded-2xl border-4 border-amber-300">
+                <div className="mx-auto">
+                    Sign Page
+                </div>
             </div>
         </div>
     )
--- frontend/app/verify/page.tsx
+++ frontend/app/verify/page.tsx
@@ -2,9 +2,11 @@
 
 export default function VerifyPage() {
     return(
-        <div id="Keys" className="relative h-screen w-full bg-black">
-            <div>
-                Verify Page
+        <div id="Verify" className="pages">
+            <div className=" flex bg-blue-500 h-full items-center text-black rounded-2xl border-4 border-amber-300">
+                <div className="mx-auto">
+                    Verify Page
+                </div>
             </div>
         </div>
     )
